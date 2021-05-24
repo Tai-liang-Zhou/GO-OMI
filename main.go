9139b6fc4815e080efa537fc612db211f7818b0b
TLCHOUD <ga731852@gmail.com>
2021-05-25 01:00:54 +0800

    add robotgo function

--- main.go
+++ main.go
@@ -1,86 +1,99 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/go-vgo/robotgo"
-	hook "github.com/robotn/gohook"
 )
 
-func addEvent() {
-
-	eventHook := robotgo.Start()
-
-	var e hook.Event
-	var key []string
-
-	fmt.Println("--- Please press ctrl + shift + q to stop hook ---")
-	fmt.Println("--- Please press w---")
-
-	for e = range eventHook {
-		if e.Kind == hook.KeyDown {
-			fmt.Println(key)
-
-			switch key {
-			case "W":
-				fmt.Println("--- Please press w---")
-			case "w":
-				fmt.Println("--- Please press ctrl + shift + q to stop hook ---")
-			default:
-				fmt.Printf("pressed %s \n", key)
-			}
-
-		}
-	}
-
-	// robotgo.EventHook(hook.KeyDown, []string{"q", "ctrl", "shift"}, func(e hook.Event) {
-	// 	fmt.Println("ctrl-shift-q")
-	// 	robotgo.EventEnd()
-	// })
-
-	// fmt.Println("--- Please press w---")
-	// robotgo.EventHook(hook.KeyDown, []string{"w"}, func(e hook.Event) {
-	// 	robotgo.TypeStrDelay("test", 2)
-	// })
-
-	// s := robotgo.EventStart()
-	// <-robotgo.EventProcess(s)
-}
-
-// func addMouse() {
-// 	fmt.Println("--- Please press left mouse button to see it's position and the right mouse button to exit ---")
-// 	robotgo.EventHook(hook.MouseDown, []string{}, func(e hook.Event) {
-// 		if e.Button == hook.MouseMap["left"] {
-// 			fmt.Printf("mouse left @ %v - %v\n", e.X, e.Y)
-// 		} else if e.Button == hook.MouseMap["right"] {
-// 			robotgo.EventEnd()
-// 		}
-// 	})
-
-// 	s := robotgo.EventStart()
-// 	<-robotgo.EventProcess(s)
-// }
-
-func lowLevel() {
-	////////////////////////////////////////////////////////////////////////////////
-	// Global event listener
-	////////////////////////////////////////////////////////////////////////////////
-	fmt.Println("Press q to stop event gathering")
-	evChan := robotgo.EventStart()
-	for e := range evChan {
-		fmt.Println(e)
-		if e.Keychar == 'q' {
-			robotgo.EventEnd()
-			// break
-		}
-	}
-}
-
 func main() {
-	fmt.Println("test begin...")
-	addEvent()
+	fmt.Println("OMI AUTO TEST TOOL...")
+	fmt.Println("請選擇要執行function 1. 操作截圖 2. open csv")
 
-	// addMouse()
+	var a int
+	fmt.Scanln(&a)
+	switch a {
+	case 1:
+		robotgo.MoveMouse(762, 1425)
+		robotgo.MouseClick()
+		robotgo.Sleep(1)
+		robotgo.KeyTap("f2", "shift")
+	case 2:
+		ReadCsv()
+	default:
+	}
 
-	lowLevel()
+}
+
+func ReadCsv() {
+	FilePath := "OMI.csv"
+	file, err := os.OpenFile(FilePath, os.O_RDONLY, 0777)
+	if err != nil {
+		log.Fatalln("找不到檔案路徑", FilePath, err)
+	}
+
+	csv_data := csv.NewReader(file)
+	csv_data.Comma = ','
+	m := map[string][]string{}
+
+	for {
+		// var action []string
+		record, err := csv_data.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+		m[record[0]] = record
+
+	}
+
+	fmt.Println("請選擇要執行 OMI FUCNTION")
+	for key, value := range m {
+		fmt.Printf("FUNCTION %s, Task Step : %s\n", key, value)
+
+	}
+	var task string
+	fmt.Scanln(&task)
+	for _, value := range m[task][1:] {
+		exe_robotkey(value)
+	}
+
+}
+
+func exe_robotkey(key string) {
+	if strings.ContainsAny(key, "()") {
+		go_key := key[0:strings.Index(key, "(")]
+		value := key[strings.Index(key, "(")+1 : strings.Index(key, ")")]
+
+		fmt.Println(go_key, value)
+		robotgo_function(go_key, value)
+	} else {
+		fmt.Println(key)
+	}
+
+}
+
+func robotgo_function(key string, value string) {
+	switch key {
+	case "key":
+		robotgo.TypeStr(value)
+	case "sleep":
+		v, err := strconv.Atoi(value)
+		if err != nil {
+			log.Fatal(err)
+		}
+		robotgo.Sleep(v)
+
+	default:
+		fmt.Println("fuck")
+	}
+
 }
